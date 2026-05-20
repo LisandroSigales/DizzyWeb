69ba81811d9b68110c9e9d6de3263fa0dcd09644
LisandroSigales <lisandr868@gmail.com>
2026-05-20 16:57:15 -0300

    Galeria

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const container = document.getElementById("myCarousel");
+const options = {
+  // Your custom options
+};
+
+Carousel(container, options).init();
+
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
+});
